76ce939f5d684d0cc7564b11d368379500bcf191
VIVEK KOKAMBE <vivekkokambe612@gmail.com>
2025-02-06 03:18:03 +0530

    image slider updated
    new image slider

--- src/components/CollegeClub/CollegeClub.js
+++ src/components/CollegeClub/CollegeClub.js
@@ -1,149 +1,239 @@
-import React from "react";
-// import logo from "../../assets/img/hack-overflow-logo.png";
-// import fir from "../../assets/img/first.png"
-// import banneriamge from "../../assets/img/hackathon.jpg";
-// import image1 from "../../assets/img/college-club-events/image1.jpeg";
-// import image2 from "../../assets/img/college-club-events/image2.jpeg";
-// import image3 from "../../assets/img/college-club-events/image3.jpg";
-// import image4 from "../../assets/img/college-club-events/image4.jpg";
-// import image5 from "../../assets/img/college-club-events/image5.jpg";
-// import image6 from "../../assets/img/college-club-events/image6.jpg";
-// import image7 from "../../assets/img/college-club-events/image7.jpg";
-// import image8 from "../../assets/img/college-club-events/image8.jpg";
-// import image9 from "../../assets/img/college-club-events/image9.jpg";
-// import image10 from "../../assets/img/college-club-events/image10.jpg";
-// import image11 from "../../assets/img/college-club-events/image11.jpg";
-// import image12 from "../../assets/img/college-club-events/image12.jpg";
-// import image13 from "../../assets/img/college-club-events/image13.jpg";
-// import image14 from "../../assets/img/college-club-events/image14.png";
-// import image15 from "../../assets/img/college-club-events/image15.png";
-// import image16 from "../../assets/img/college-club-events/image16.png";
-// import image17 from "../../assets/img/college-club-events/image17.JPG";
-// import image18 from "../../assets/img/college-club-events/image18.JPG";
-// import image19 from "../../assets/img/college-club-events/image19.jpg";
-// import image20 from "../../assets/img/college-club-events/image20.jpeg";
-// import image21 from "../../assets/img/college-club-events/image21.JPG";
+// import React from "react";
+ 
 
 // NEW IMAGES IMPORT
-import image1 from "../../assets/img/Events/college-club-events-23/image1.webp";
-import image2 from "../../assets/img/Events/college-club-events-23/image2.webp";
-import image3 from "../../assets/img/Events/college-club-events-23/image3.webp";
-import image4 from "../../assets/img/Events/college-club-events-23/image4.webp";
-import image5 from "../../assets/img/Events/college-club-events-23/image5.webp";
-import image6 from "../../assets/img/Events/college-club-events-23/image6.webp";
-import image7 from "../../assets/img/Events/college-club-events-23/image7.webp";
-import "./CollegeClub.css";
-import Carousel from "react-bootstrap/Carousel";
-import { Link } from "react-router-dom";
+// import image1 from "../../assets/img/Events/college-club-events-23/image1.webp";
+// import image2 from "../../assets/img/Events/college-club-events-23/image2.webp";
+// import image3 from "../../assets/img/Events/college-club-events-23/image3.webp";
+// import image4 from "../../assets/img/Events/college-club-events-23/image4.webp";
+// import image5 from "../../assets/img/Events/college-club-events-23/image5.webp";
+// import image6 from "../../assets/img/Events/college-club-events-23/image6.webp";
+// import image7 from "../../assets/img/Events/college-club-events-23/image7.webp";
+// import "./CollegeClub.css";
+// import Carousel from "react-bootstrap/Carousel";
+// import { Link } from "react-router-dom";
 
-const CollegeClub = () => {
+// const CollegeClub = () => {
+//   return (
+//     <>
+//       <section className="top pb-6">
+//         <div className="relative topContainer flex flex-col justify-center items-center">
+//           {/* <div class="absolute h-[4px] bg-[#7b1072] w-24" /> */}
+//           <h1
+//             id="orange-pink"
+//             className="pt-2 text-[30px] sm:text-[36px] md:text-[35px] lg:text-[45px]"
+//             style={{
+//               fontFamily: "Poppins,sans-serif",
+//               fontWeight: 700,
+//               textAlign: "center",
+//             }}
+//           >
+//             Glimpses of Hackoverflow 1.0
+//           </h1>
+//           <div className="Description mt-4 md:mt-2">
+//             <div style={{ display: "flex", justifyContent: "center" }}>
+//               <Carousel
+//                 interval={2000}
+//                 className="border-white d-block h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//               >
+//                 <Carousel.Item>
+//                   <img
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image1}
+//                     alt="First slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image2}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image3}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image4}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image5}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image6}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                   <img
+
+//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
+//                     src={image7}
+//                     alt="Second slide"
+//                   />
+//                 </Carousel.Item>
+//               </Carousel>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <div className="navbar-text flex justify-center">
+//         <Link to="/gallery">
+//           <button className="SeeMoreButton bg-[#9B96B3] hover:bg-[#891A98] text-white font-bold py-2 px-4 rounded">
+//             See More...
+//           </button>
+//         </Link>
+//       </div>
+ 
+//     </>
+//   );
+// };
+
+// export default CollegeClub;
+
+
+
+
+
+
+import React,{useState} from 'react';
+import {Swiper,SwiperSlide} from "swiper/react";
+
+
+ 
+import 'swiper/css';
+import './CollegeClub.css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+import {EffectCoverflow,Pagination,Navigation,Autoplay  } from "swiper/modules";
+
+ 
+ 
+import slide_image_1 from "../../assets/img/Events/college-club-events-23/image1.webp";
+import slide_image_2 from "../../assets/img/Events/college-club-events-23/image2.webp";
+import slide_image_3 from "../../assets/img/Events/college-club-events-23/image3.webp";
+import slide_image_4 from "../../assets/img/Events/college-club-events-23/image4.webp";
+import slide_image_5 from "../../assets/img/Events/college-club-events-23/image5.webp";
+import slide_image_6 from "../../assets/img/Events/college-club-events-23/image6.webp";
+ 
+ 
+function App() {
+   
+  
+  
+  const [swiperInstance, setSwiperInstance] = useState(null);
+
+  // Handle autoplay stop on hover
+  const handleMouseEnter = () => {
+    if (swiperInstance) swiperInstance.autoplay.stop();
+  };
+
+  // Handle autoplay resume when mouse leaves
+  const handleMouseLeave = () => {
+    if (swiperInstance) swiperInstance.autoplay.start();
+  };
+
   return (
     <>
-      <section className="top pb-6">
-        <div className="relative topContainer flex flex-col justify-center items-center">
-          {/* <div class="absolute h-[4px] bg-[#7b1072] w-24" /> */}
-          <h1
-            id="orange-pink"
-            className="pt-2 text-[30px] sm:text-[36px] md:text-[35px] lg:text-[45px]"
-            style={{
-              fontFamily: "Poppins,sans-serif",
-              fontWeight: 700,
-              textAlign: "center",
-            }}
-          >
-            Glimpses of Hackoverflow 1.0
-          </h1>
-          <div className="Description mt-4 md:mt-2">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Carousel
-                interval={2000}
-                className="border-white d-block h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-              >
-                <Carousel.Item>
-                  <img
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image1}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image2}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image3}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image4}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image5}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image6}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image7}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-            </div>
+    <div className="SliderMainSection"
+   
+    >
+    <div className="SliderContentContainer">
+      <h1 className="SliderHeading">Glimpses of HackOverflow 2.0</h1>
+      <Swiper
+       
+        onSwiper={setSwiperInstance}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+         
+        autoplay={{
+          delay: 2000,  // Slides change every 3 seconds
+        }}
+        speed={800}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation,Autoplay]}
+        className="swiper_container"
+        onMouseEnter={handleMouseEnter} // Stop autoplay on hover
+        onMouseLeave={handleMouseLeave}
+      >
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        
+        
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
           </div>
+          
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          
         </div>
-      </section>
-      <div className="navbar-text flex justify-center">
-        <Link to="/gallery">
-          <button className="SeeMoreButton bg-[#9B96B3] hover:bg-[#891A98] text-white font-bold py-2 px-4 rounded">
-            See More...
-          </button>
-        </Link>
-      </div>
-
-      {/* <div class="bg-cover bg-center h-screen w-full md:h-64 md:w-auto" style={{ backgroundImage: `url(${banneriamge})` }}>
-                <div class="container mx-auto h-full md:h-64">
-                    <ImageSlider effectDelay={500} autoPlayDelay={1000}>
-                        <Slide>
-                            <img alt="img1" class="w-full h-64 object-cover" src={logo} />
-                        </Slide>
-                        <Slide>
-                            <img alt="img2" class="w-full h-64 object-cover" src={fir} />
-                        </Slide>
-                        <Slide>
-                            <img alt="img3" class="w-full h-64 object-cover" src={logo} />
-                        </Slide>
-                    </ImageSlider>
-                </div>
-            </div> */}
+        <div className="swiper-pagination"></div>
+      </Swiper>
+    </div>
+    </div>
+     
+    
     </>
+    
   );
-};
+}
 
-export default CollegeClub;
+export default App;
