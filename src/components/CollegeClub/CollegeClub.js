13d60a1105336745b36050f42a703d2ec5244ba9
VIVEK KOKAMBE <vivekkokambe612@gmail.com>
2025-02-06 18:18:36 +0530

    revised image slider update

--- src/components/CollegeClub/CollegeClub.js
+++ src/components/CollegeClub/CollegeClub.js
@@ -1,119 +1,3 @@
-// import React from "react";
- 
-
-// NEW IMAGES IMPORT
-// import image1 from "../../assets/img/Events/college-club-events-23/image1.webp";
-// import image2 from "../../assets/img/Events/college-club-events-23/image2.webp";
-// import image3 from "../../assets/img/Events/college-club-events-23/image3.webp";
-// import image4 from "../../assets/img/Events/college-club-events-23/image4.webp";
-// import image5 from "../../assets/img/Events/college-club-events-23/image5.webp";
-// import image6 from "../../assets/img/Events/college-club-events-23/image6.webp";
-// import image7 from "../../assets/img/Events/college-club-events-23/image7.webp";
-// import "./CollegeClub.css";
-// import Carousel from "react-bootstrap/Carousel";
-// import { Link } from "react-router-dom";
-
-// const CollegeClub = () => {
-//   return (
-//     <>
-//       <section className="top pb-6">
-//         <div className="relative topContainer flex flex-col justify-center items-center">
-//           {/* <div class="absolute h-[4px] bg-[#7b1072] w-24" /> */}
-//           <h1
-//             id="orange-pink"
-//             className="pt-2 text-[30px] sm:text-[36px] md:text-[35px] lg:text-[45px]"
-//             style={{
-//               fontFamily: "Poppins,sans-serif",
-//               fontWeight: 700,
-//               textAlign: "center",
-//             }}
-//           >
-//             Glimpses of Hackoverflow 1.0
-//           </h1>
-//           <div className="Description mt-4 md:mt-2">
-//             <div style={{ display: "flex", justifyContent: "center" }}>
-//               <Carousel
-//                 interval={2000}
-//                 className="border-white d-block h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//               >
-//                 <Carousel.Item>
-//                   <img
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image1}
-//                     alt="First slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image2}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image3}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image4}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image5}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image6}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                   <img
-
-//                     className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-//                     src={image7}
-//                     alt="Second slide"
-//                   />
-//                 </Carousel.Item>
-//               </Carousel>
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//       <div className="navbar-text flex justify-center">
-//         <Link to="/gallery">
-//           <button className="SeeMoreButton bg-[#9B96B3] hover:bg-[#891A98] text-white font-bold py-2 px-4 rounded">
-//             See More...
-//           </button>
-//         </Link>
-//       </div>
- 
-//     </>
-//   );
-// };
-
-// export default CollegeClub;
-
-
-
-
-
 
 import React,{useState} from 'react';
 import {Swiper,SwiperSlide} from "swiper/react";
@@ -157,10 +41,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-    <div className="SliderMainSection"
-   
-    >
+    <div className="SliderMainSection">
     <div className="SliderContentContainer">
       <h1 className="SliderHeading">Glimpses of HackOverflow 2.0</h1>
       <Swiper
@@ -214,130 +95,22 @@ function App() {
         <SwiperSlide onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <img src={slide_image_5} alt="slide_image" />
         </SwiperSlide>
-        
-        
+      
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
             <ion-icon name="arrow-back-outline"></ion-icon>
-=======
-      <section className="top pb-6">
-        <div className="relative topContainer flex flex-col justify-center items-center">
-          {/* <div class="absolute h-[4px] bg-[#7b1072] w-24" /> */}
-          <h1
-             id="green-yellow"
-            className="pt-2 text-[30px] sm:text-[36px] md:text-[35px] lg:text-[45px]"
-            style={{
-              fontFamily: "Poppins,sans-serif",
-              fontWeight: 700,
-              textAlign: "center",
-            }}
-          >
-            Glimpses of Hackoverflow 2.0
-          </h1>
-          <div className="Description mt-4 md:mt-2">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Carousel
-                interval={2000}
-                className="border-white d-block h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-              >
-                <Carousel.Item>
-                  <img
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image1}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image2}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image3}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image4}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image5}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image6}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-
-                    className="d-block w-screen h-[235px] sm:h-[450px] lg:h-[550px] xl:h-[550px]"
-                    src={image7}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-            </div>
->>>>>>> 96c3498768b14098b4e08c1d7880476d71cc18b5
           </div>
-          
           <div className="swiper-button-next slider-arrow">
             <ion-icon name="arrow-forward-outline"></ion-icon>
           </div>
           
         </div>
-<<<<<<< HEAD
         <div className="swiper-pagination"></div>
       </Swiper>
     </div>
     </div>
      
     
-=======
-      </section>
-      <div className="navbar-text flex justify-center">
-        <Link to="/gallery">
-          <button className="SeeMoreButton bg-[#9B96B3] hover:bg-[#5B8F81] text-white font-bold py-2 px-4 rounded">
-            See More...
-          </button>
-        </Link>
-      </div>
-
-      {/* <div class="bg-cover bg-center h-screen w-full md:h-64 md:w-auto" style={{ backgroundImage: `url(${banneriamge})` }}>
-                <div class="container mx-auto h-full md:h-64">
-                    <ImageSlider effectDelay={500} autoPlayDelay={1000}>
-                        <Slide>
-                            <img alt="img1" class="w-full h-64 object-cover" src={logo} />
-                        </Slide>
-                        <Slide>
-                            <img alt="img2" class="w-full h-64 object-cover" src={fir} />
-                        </Slide>
-                        <Slide>
-                            <img alt="img3" class="w-full h-64 object-cover" src={logo} />
-                        </Slide>
-                    </ImageSlider>
-                </div>
-            </div> */}
->>>>>>> 96c3498768b14098b4e08c1d7880476d71cc18b5
     </>
     
   );
